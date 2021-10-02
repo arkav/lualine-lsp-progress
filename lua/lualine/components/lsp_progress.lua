6a2bfef46b8610b40be65f7e7a60ed6024e3b0fe
Arkav <me@arkav.dev>
2021-10-01 15:26:12 -1000

    fix inconsistant indenation

--- lua/lualine/components/lsp_progress.lua
+++ lua/lualine/components/lsp_progress.lua
@@ -6,12 +6,12 @@ local LspProgress = require('lualine.component'):new()
 -- LuaFormatter off
 LspProgress.default = {
 	colors = {
-	  percentage  = '#ffffff',
-	  title  = '#ffffff',
-	  message  = '#ffffff',
-	  spinner = '#008080',
-	  lsp_client_name = '#c678dd',
-	  use = true,
+	percentage  = '#ffffff',
+		title  = '#ffffff',
+		message  = '#ffffff',
+		spinner = '#008080',
+		lsp_client_name = '#c678dd',
+		use = true,
 	},
 	separators = {
 		component = ' ',
@@ -33,53 +33,35 @@ LspProgress.default = {
 
 -- Initializer
 LspProgress.new = function(self, options, child)
-  local new_lsp_progress = self._parent:new(options, child or LspProgress)
+	local new_lsp_progress = self._parent:new(options, child or LspProgress)
 
-  new_lsp_progress.options.colors = vim.tbl_extend('force', LspProgress.default.colors, 
-										new_lsp_progress.options.colors or {})
-  new_lsp_progress.options.separators = vim.tbl_deep_extend('force', LspProgress.default.separators, 
-										new_lsp_progress.options.separators or {})
-  new_lsp_progress.options.display_components = new_lsp_progress.options.display_components or LspProgress.default.display_components
-  new_lsp_progress.options.timer = vim.tbl_extend('force', LspProgress.default.timer, 
-										new_lsp_progress.options.timer or {})
-  new_lsp_progress.options.spinner_symbols = vim.tbl_extend('force', LspProgress.default.spinner_symbols, 
-										new_lsp_progress.options.spinner_symbols or {})
-  new_lsp_progress.options.message = vim.tbl_extend('force', LspProgress.default.message, 
-										new_lsp_progress.options.message or {})
+	new_lsp_progress.options.colors = vim.tbl_extend('force', LspProgress.default.colors, new_lsp_progress.options.colors or {})
+	new_lsp_progress.options.separators = vim.tbl_deep_extend('force', LspProgress.default.separators, new_lsp_progress.options.separators or {})
+	new_lsp_progress.options.display_components = new_lsp_progress.options.display_components or LspProgress.default.display_components
+	new_lsp_progress.options.timer = vim.tbl_extend('force', LspProgress.default.timer, new_lsp_progress.options.timer or {})
+	new_lsp_progress.options.spinner_symbols = vim.tbl_extend('force', LspProgress.default.spinner_symbols, new_lsp_progress.options.spinner_symbols or {})
+	new_lsp_progress.options.message = vim.tbl_extend('force', LspProgress.default.message, new_lsp_progress.options.message or {})
 
-  new_lsp_progress.highlights = { percentage = '', title = '', message = '' }
-  if new_lsp_progress.options.colors.use then
-    new_lsp_progress.highlights.title = highlight.create_component_highlight_group(
-	  { fg = new_lsp_progress.options.colors.title }, 'lspprogress_title', new_lsp_progress.options
-    )
-    new_lsp_progress.highlights.percentage = highlight.create_component_highlight_group(
-	  { fg = new_lsp_progress.options.colors.percentage }, 'lspprogress_percentage', new_lsp_progress.options
-    )
-    new_lsp_progress.highlights.message = highlight.create_component_highlight_group(
-	  { fg = new_lsp_progress.options.colors.message }, 'lspprogress_message', new_lsp_progress.options
-    )
-    new_lsp_progress.highlights.spinner = highlight.create_component_highlight_group(
-	  { fg = new_lsp_progress.options.colors.spinner }, 'lspprogress_spinner', new_lsp_progress.options
-    )
-    new_lsp_progress.highlights.lsp_client_name = highlight.create_component_highlight_group(
-	  { fg = new_lsp_progress.options.colors.lsp_client_name }, 'lspprogress_lsp_client_name', new_lsp_progress.options
-    )
-  end
+	new_lsp_progress.highlights = { percentage = '', title = '', message = '' }
+	if new_lsp_progress.options.colors.use then
+		new_lsp_progress.highlights.title = highlight.create_component_highlight_group({ fg = new_lsp_progress.options.colors.title }, 'lspprogress_title', new_lsp_progress.options)
+		new_lsp_progress.highlights.percentage = highlight.create_component_highlight_group({ fg = new_lsp_progress.options.colors.percentage }, 'lspprogress_percentage', new_lsp_progress.options)
+		new_lsp_progress.highlights.message = highlight.create_component_highlight_group({ fg = new_lsp_progress.options.colors.message }, 'lspprogress_message', new_lsp_progress.options)
+		new_lsp_progress.highlights.spinner = highlight.create_component_highlight_group({ fg = new_lsp_progress.options.colors.spinner }, 'lspprogress_spinner', new_lsp_progress.options)
+		new_lsp_progress.highlights.lsp_client_name = highlight.create_component_highlight_group({ fg = new_lsp_progress.options.colors.lsp_client_name }, 'lspprogress_lsp_client_name', new_lsp_progress.options)
+	end
+	-- Setup callback to get updates from the lsp to update lualine.
 
+	new_lsp_progress:register_progress()
+	-- No point in setting spinner callbacks if it is not displayed.
+	for _, display_component in pairs(new_lsp_progress.options.display_components) do
+		if display_component == 'spinner' then
+			new_lsp_progress:setup_spinner()
+		break
+	end
+end
 
-
-  -- Setup callback to get updates from the lsp to update lualine.
-
-  new_lsp_progress:register_progress()
-  -- No point in setting spinner callbacks if it is not displayed.
-  for _, display_component in pairs(new_lsp_progress.options.display_components) do
-	  if display_component == 'spinner' then
-		  new_lsp_progress:setup_spinner()
-		  break
-	  end
-  end
-
-  return new_lsp_progress
+return new_lsp_progress
 end
 
 LspProgress.update_status = function(self)
@@ -89,66 +71,66 @@ end
 
 
 LspProgress.register_progress = function(self)
-  self.clients = {}
+	self.clients = {}
 
-  self.progress_callback = function (_, _, msg, client_id_int)
-  	local key = msg.token
-  	local val = msg.value
-	local client_id = tostring(client_id_int)
+	self.progress_callback = function (_, _, msg, client_id_int)
+		local key = msg.token
+		local val = msg.value
+		local client_id = tostring(client_id_int)
 
 
-  	if key then
-		if self.clients[client_id] == nil then
-			self.clients[client_id] = { progress = {}, name = vim.lsp.get_client_by_id(client_id_int).name }
-		end
-		local progress_collection = self.clients[client_id].progress
-		if progress_collection[key] == nil then
-			progress_collection[key] = { title = nil, message = nil, percentage = nil }
-		end
-
-		local progress = progress_collection[key]
-
-  		if val then
-  			if val.kind == 'begin' then
-				progress.title = val.title
-				progress.message = self.options.message.commenced
-  			end
-			if val.kind == 'report' then
-				if val.percentage then
-					progress.percentage = val.percentage
-				end
-				if val.message then
-					progress.message = val.message
-				end
-  			end
-			if val.kind == 'end' then
-				if progress.percentage then
-					progress.percentage = '100'
-				end
-				progress.message = self.options.message.completed
-				vim.defer_fn(function() 
-					if self.clients[client_id] then
-						self.clients[client_id].progress[key] = nil
-					end
-					vim.defer_fn(function()
-						local has_items = false
-						if self.clients[client_id] and self.clients[client_id].progress then
-							for _, _ in pairs(self.clients[client_id].progress) do
-								has_items = 1
-								break
-							end
-						end
-						if has_items == false then
-							self.clients[client_id] = nil
-						end
-					end, self.options.timer.lsp_client_name_enddelay)
-				end, self.options.timer.progress_enddelay)
+		if key then
+			if self.clients[client_id] == nil then
+				self.clients[client_id] = { progress = {}, name = vim.lsp.get_client_by_id(client_id_int).name }
 			end
-  		end
-  	end
-  end
+			local progress_collection = self.clients[client_id].progress
+			if progress_collection[key] == nil then
+				progress_collection[key] = { title = nil, message = nil, percentage = nil }
+			end
 
-  vim.lsp.handlers["$/progress"] = self.progress_callback
+			local progress = progress_collection[key]
+
+			if val then
+				if val.kind == 'begin' then
+					progress.title = val.title
+					progress.message = self.options.message.commenced
+				end
+				if val.kind == 'report' then
+					if val.percentage then
+						progress.percentage = val.percentage
+					end
+					if val.message then
+						progress.message = val.message
+					end
+				end
+				if val.kind == 'end' then
+					if progress.percentage then
+						progress.percentage = '100'
+					end
+					progress.message = self.options.message.completed
+					vim.defer_fn(function() 
+						if self.clients[client_id] then
+							self.clients[client_id].progress[key] = nil
+						end
+						vim.defer_fn(function()
+							local has_items = false
+							if self.clients[client_id] and self.clients[client_id].progress then
+								for _, _ in pairs(self.clients[client_id].progress) do
+									has_items = 1
+									break
+								end
+							end
+							if has_items == false then
+								self.clients[client_id] = nil
+							end
+						end, self.options.timer.lsp_client_name_enddelay)
+					end, self.options.timer.progress_enddelay)
+				end
+			end
+		end
+	end
+
+	vim.lsp.handlers["$/progress"] = self.progress_callback
 end
 
 LspProgress.update_progress = function(self)
@@ -198,7 +180,7 @@ LspProgress.update_progress_components = function(self, result, display_componen
 				if progress[i] and progress[i] ~= '' then
 					if options.colors.use then
 						table.insert(d, highlight.component_format_highlight(self.highlights[i]) .. options.separators[i].pre .. progress[i] .. options.separators[i].post)
-					else 
+					else
 						table.insert(d, options.separators[i].pre .. progress[i] .. options.separators[i].post)
 					end
 				end
